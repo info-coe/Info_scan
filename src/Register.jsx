537081936ce89d4dcc59ff24e142f51884c515e4
infocoe <coeteam@infomericainc.in>
2024-08-13 18:46:10 +0530

    updated register

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import scan1 from "./Images/scan1.jpg";
+import scan2 from "./Images/scan2.jpg";
+import scan3 from "./Images/scan3.jpg";
 
 const Register = () => {
   const [confirmpassword, setConfirmpassword] = useState("");
@@ -23,7 +26,7 @@ const Register = () => {
     }));
     // console.log(CryptoJS.MD5(event.target.value).toString())
   };
- 
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const { email, phone, password } = values;
@@ -31,7 +34,7 @@ const Register = () => {
     //   setError("This Email already Registered");
     // } else if (userdetails.some((user) => user.phone.toString() === phone)) {
     //   setError("Phone number already exists");
-    // } else 
+    // } else
     if (password !== confirmpassword) {
       setError("Passwords do not match");
     } else {
@@ -42,7 +45,7 @@ const Register = () => {
         );
       } else {
         setError("");
-        sessionStorage.setItem("registered_user",JSON.stringify(values));
+        sessionStorage.setItem("registered_user", JSON.stringify(values));
         navigate("/main");
         // values.password = CryptoJS.MD5(values.password).toString();
         // navigate("/emailverification", { state: { values } });
@@ -61,184 +64,193 @@ const Register = () => {
   return (
     <div className="fullscreen">
       <main>
-        <div className="p-2 ps-lg-5 pe-lg-5 mb-5">
-          <div className="col-xs-12 col-md-12 col-lg-12">
-            <form method="post" onSubmit={handleSubmit}>
-              <div>
-                <div>
-                  <h1 className="text-center fs-3">Register</h1>
+        <div className=" d-lg-flex gap-5 align-items-center" style={{minHeight:'100vh'}}>
+          {/* <div className=""> */}
+       
+            
+                <div className="col-lg-7">
+                  <img
+                    src={scan2}
+                    // className="img-fluid"
+                    width="100%"
+                    alt="Sample"
+                  />
                 </div>
-                <hr />
-                <div className="form-group d-md-flex justify-content-center mt-4 mb-2">
-                  <label
-                    className="control-label col-sm-2 col-md-2 fw-bold"
-                    htmlFor="firstname"
-                  >
-                    First Name
-                  </label>
-                  <div className="d-flex col-sm-6 col-md-4 col-xs-12">
-                    <input
-                      className="form-control mb-2"
-                      type="text"
-                      id="firstname"
-                      name="firstname"
-                      onChange={handleInput}
-                      placeholder="Enter First Name"
-                      pattern="[A-Z][a-z]*\s*\w*"
-                      title="First letter should be uppercase, remaining letters are lowercase. No special characters"
-                      required
-                    />
-                    <span className="text-danger fs-4"> &nbsp;*</span>
-                  </div>
+                <div className="col-lg-4 ">
+                  <form method="post" onSubmit={handleSubmit}>
+                    <div>
+                      <div>
+                        <h1 className="text-center fs-3">Register</h1>
+                      </div>
+                      {/* <hr /> */}
+                      <div className="form-group mt-4 mb-2">
+                        <label className="control-label fw-bold" htmlFor="firstname">
+                          First Name
+                        </label>
+                        <div className="d-flex">
+                          <input
+                            className="form-control mb-2"
+                            type="text"
+                            id="firstname"
+                            name="firstname"
+                            onChange={handleInput}
+                            placeholder="Enter First Name"
+                            pattern="[A-Z][a-z]*\s*\w*"
+                            title="First letter should be uppercase, remaining letters are lowercase. No special characters"
+                            required
+                          />
+                          <span className="text-danger fs-4"> &nbsp;*</span>
+                        </div>
+                      </div>
+                      <div className="form-group mb-2">
+                        <label className="control-label fw-bold" htmlFor="lastname">
+                          Last Name
+                        </label>
+                        <div className="d-flex ">
+                          <input
+                            className="form-control mb-2"
+                            type="text"
+                            id="lastname"
+                            name="lastname"
+                            onChange={handleInput}
+                            placeholder="Enter Last Name"
+                            pattern="[A-Z][a-z]*\s*\w*"
+                            title="First letter should be uppercase, remaining letters are lowercase. No special characters"
+                            required
+                          />
+                          <span className="text-danger fs-4"> &nbsp;*</span>
+                        </div>
+                      </div>
+                      <div className="form-group mb-2">
+                        <label className="control-label fw-bold" htmlFor="email">
+                          Email
+                        </label>
+                        <div className="d-flex ">
+                          <input
+                            className="form-control mb-2"
+                            type="email"
+                            id="email"
+                            name="email"
+                            onChange={handleInput}
+                            placeholder="Enter Email"
+                            required
+                          />
+                          <span className="text-danger fs-4"> &nbsp;*</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="form-group mb-2">
+                        <label className="control-label fw-bold" htmlFor="phone">
+                          Phone
+                        </label>
+                        <div className="d-flex ">
+                          <input
+                            className="form-control mb-2"
+                            type="tel"
+                            id="phone"
+                            name="phone"
+                            onChange={handleInput}
+                            placeholder="Enter Phone Number"
+                            pattern="[0-9]{10}"
+                            title="10 digit numeric value only"
+                            minLength={10}
+                            maxLength={10}
+                            required
+                          />
+                          <span className="text-danger fs-4"> &nbsp;*</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="form-group mb-2">
+                        <label className="control-label fw-bold" htmlFor="password">
+                          Password
+                        </label>
+                        <div className="d-flex  passwordgroup">
+                          <input
+                            className="form-control mb-2"
+                            type={showPassword ? "text" : "password"}
+                            id="password"
+                            name="password"
+                            onChange={handleInput}
+                            placeholder="Enter Password"
+                            required
+                            pattern="^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).{8,}$"
+                            title="Password must contain at least 8 characters, including one number, one letter, and one special character."
+                          />
+                          <button
+                            type="button"
+                            id="btnToggle"
+                            className="toggle12"
+                            onClick={handleTogglePassword}
+                          >
+                            <i
+                              className={`bi ${
+                                showPassword ? "bi-eye-slash" : "bi-eye"
+                              }`}
+                            ></i>
+                          </button>
+                          <span className="text-danger fs-4"> &nbsp;*</span>
+                        </div>
+                      </div>
+                      <div className="form-group mb-2">
+                        <label
+                          className="control-label fw-bold"
+                          htmlFor="confirmpassword"
+                        >
+                          Confirm Password
+                        </label>
+                        <div className="d-flex  passwordgroup">
+                          <input
+                            className="form-control mb-2"
+                            type={showConfirmPassword ? "text" : "password"}
+                            id="confirmpassword"
+                            name="confirmpassword"
+                            onChange={(e) =>
+                              setConfirmpassword(e.currentTarget.value)
+                            }
+                            placeholder="Enter Confirm Password"
+                            required
+                          />
+                          <button
+                            type="button"
+                            id="btnToggle"
+                            className="toggle12"
+                            onClick={handleToggleConfirmPassword}
+                          >
+                            <i
+                              className={`bi ${
+                                showConfirmPassword ? "bi-eye-slash" : "bi-eye"
+                              }`}
+                            ></i>
+                          </button>
+                          <span className="text-danger fs-4"> &nbsp;*</span>
+                        </div>
+                      </div>
+                    </div>
+                    {/* Error message */}
+                    {error && (
+                      <div className="text-danger text-center mb-3">
+                        {error}
+                      </div>
+                    )}
+                    <div className="form-group ">
+                      <div className=""></div>
+                      <div className=" text-center">
+                        <button
+                          type="submit"
+                          className="btn btn-primary register-next-step-button w-50 mt-3"
+                        >
+                          Register
+                        </button>
+                      </div>
+                    </div>
+                  </form>
                 </div>
-                <div className="form-group  d-md-flex justify-content-center mt-2 mb-2">
-                  <label
-                    className="control-label col-sm-2 col-md-2 fw-bold"
-                    htmlFor="lastname"
-                  >
-                    Last Name
-                  </label>
-                  <div className="d-flex col-sm-6 col-md-4 col-xs-12">
-                    <input
-                      className="form-control mb-2"
-                      type="text"
-                      id="lastname"
-                      name="lastname"
-                      onChange={handleInput}
-                      placeholder="Enter Last Name"
-                      pattern="[A-Z][a-z]*\s*\w*"
-                      title="First letter should be uppercase, remaining letters are lowercase. No special characters"
-                      required
-                    />
-                    <span className="text-danger fs-4"> &nbsp;*</span>
-                  </div>
-                </div>
-                <div className="form-group  d-md-flex justify-content-center mt-2 mb-2">
-                  <label
-                    className="control-label col-sm-2 col-md-2 fw-bold"
-                    htmlFor="email"
-                  >
-                    Email
-                  </label>
-                  <div className="d-flex col-sm-6 col-md-4 col-xs-12">
-                    <input
-                      className="form-control mb-2"
-                      type="email"
-                      id="email"
-                      name="email"
-                      onChange={handleInput}
-                      placeholder="Enter Email"
-                      required
-                    />
-                    <span className="text-danger fs-4"> &nbsp;*</span>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div className="form-group  d-md-flex justify-content-center mt-2 mb-2">
-                  <label
-                    className="control-label col-sm-2 col-md-2 fw-bold"
-                    htmlFor="phone"
-                  >
-                    Phone
-                  </label>
-                  <div className="d-flex col-sm-6 col-md-4 col-xs-12">
-                    <input
-                      className="form-control mb-2"
-                      type="tel"
-                      id="phone"
-                      name="phone"
-                      onChange={handleInput}
-                      placeholder="Enter Phone Number"
-                      pattern="[0-9]{10}"
-                      title="10 digit numeric value only"
-                      minLength={10}
-                      maxLength={10}
-                      required
-                    />
-                    <span className="text-danger fs-4"> &nbsp;*</span>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div className="form-group  d-md-flex justify-content-center mt-2 mb-2">
-                  <label
-                    className="control-label col-sm-2 col-md-2 fw-bold"
-                    htmlFor="password"
-                  >
-                    Password
-                  </label>
-                  <div className="d-flex col-sm-6 col-md-4 col-xs-12 passwordgroup">
-                    <input
-                      className="form-control mb-2"
-                      type={showPassword ? 'text' : 'password'}
-                      id="password"
-                      name="password"
-                      onChange={handleInput}
-                      placeholder="Enter Password"
-                      required
-                      pattern="^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).{8,}$"
-                      title="Password must contain at least 8 characters, including one number, one letter, and one special character."
-                    />
-                     <button
-                      type="button"
-                      id="btnToggle"
-                      className="toggle12"
-                      onClick={handleTogglePassword}
-                    >
-                      <i className={`bi ${showPassword ? 'bi-eye-slash' : 'bi-eye'}`}></i>
-                    </button>
-                    <span className="text-danger fs-4"> &nbsp;*</span>
-                  </div>
-                </div>
-                <div className="form-group  d-md-flex justify-content-center mt-2 mb-2">
-                  <label
-                    className="control-label col-sm-2 col-md-2 fw-bold"
-                    htmlFor="confirmpassword"
-                  >
-                    Confirm Password
-                  </label>
-                  <div className="d-flex col-sm-6 col-md-4 col-xs-12 passwordgroup">
-                    <input
-                      className="form-control mb-2"
-                      type={showConfirmPassword ? 'text' : 'password'}
-                      id="confirmpassword"
-                      name="confirmpassword"
-                      onChange={(e) =>
-                        setConfirmpassword(e.currentTarget.value)
-                      }
-                      placeholder="Enter Confirm Password"
-                      required
-                    />
-                     <button
-                      type="button"
-                      id="btnToggle"
-                      className="toggle12"
-                      onClick={handleToggleConfirmPassword}
-                    >
-                      <i className={`bi ${showConfirmPassword ? 'bi-eye-slash' : 'bi-eye'}`}></i>
-                    </button>
-                    <span className="text-danger fs-4"> &nbsp;*</span>
-                  </div>
-                </div>
-              </div>
-              {/* Error message */}
-              {error && (
-                <div className="text-danger text-center mb-3">{error}</div>
-              )}
-              <div className="form-group  d-md-flex justify-content-center">
-                <div className="col-sm-2 col-md-2"></div>
-                <div className="col-sm-6 col-md-4 col-xs-12 text-center">
-                  <button
-                    type="submit"
-                    className="btn btn-primary register-next-step-button w-50 mt-3"
-                  >
-                    Register
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+             
+           
+          {/* </div> */}
         </div>
       </main>
     </div>
